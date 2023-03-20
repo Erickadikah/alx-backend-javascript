a82ed97e4c60f9b6c1476077bd91182cd193efa6
erickadikah <erickadikah2030@gmail.com>
2023-03-20 18:22:57 +0300

    task semistandard fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,7 @@
 // Task: 1. Block scoped variables
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock (trueOrFalse) {
   const task = false;
   const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;// task is not defined
-    const task2 = false;// task2 is not defined
-  }
 
   return [task, task2];
 }
