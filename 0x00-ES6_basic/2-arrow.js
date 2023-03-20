3767d581e3373e8ce260d5d4cb7f7c777500f350
erickadikah <erickadikah2030@gmail.com>
2023-03-20 17:58:13 +0300

    ES6

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => { //remove the function keyword and add an arrow
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,3 @@
+export default function concatArray(str, arr1, arr2) {
+    return [...str, ...arr1, ...arr2];
+}
