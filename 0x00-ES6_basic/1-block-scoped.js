c32237444d81012a4b3006bdf04abfdcea333064
erickadikah <erickadikah2030@gmail.com>
2023-03-21 01:12:19 +0300

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,13 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
+
+  if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
+  }
+
   return [task, task2];
 }
