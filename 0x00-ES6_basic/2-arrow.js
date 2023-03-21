4cecb70c369cddcf40de961e4f70e0558b60ce27
erickadikah <erickadikah2030@gmail.com>
2023-03-21 03:16:06 +0300

    task2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighborhoodsList () {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = (newNeighborhood) => { // remove the function keyword and add an arrow
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
