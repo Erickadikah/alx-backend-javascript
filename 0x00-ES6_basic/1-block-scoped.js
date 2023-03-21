04793e13f3d52b0b70b3f9f4b61df09f49473c9b
erickadikah <erickadikah2030@gmail.com>
2023-03-21 03:07:41 +0300

    eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-   // eslint-disable-next-line
+    // eslint-disable-next-line no-unused-vars
     const task = true;
-  // eslint-disable-next-line
+    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
 
