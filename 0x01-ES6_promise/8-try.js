de41a5104395c38b7530dff81708afef2b4ba3f1
erickadikah <erickadikah2030@gmail.com>
2023-03-22 21:57:39 +0300

    lint

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-    if(denominator === 0) {
-        return Promise.reject(Error('cannot divide by 0'));
-    }else{
-        return Promise.resolve(numerator/denominator);
-    }
+  if (denominator === 0) {
+    return Promise.reject(Error('cannot divide by 0'));
+  }
+  return Promise.resolve(numerator / denominator);
 }
