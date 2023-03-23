a9aabd1a3c2ae21cf83f593d1e9af4f27048d339
erickadikah <erickadikah2030@gmail.com>
2023-03-23 23:13:46 +0300

    task7 && 8

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,12 +1,12 @@
 class Airport {
-    constructor(name = '', code = '') {
+  constructor(name = '', code = '') {
     this._name = name;
     this._code = code;
-    }
+  }
 
-    get [Symbol.toStringTag]() {
-        return `${this._code}`;
-    }
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
 }
 
 export default Airport;
