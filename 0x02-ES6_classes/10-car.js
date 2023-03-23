6cef5f8ad364351093bad4e2d3c61032de2e7288
erickadikah <erickadikah2030@gmail.com>
2023-03-24 00:15:05 +0300

    task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,6 +5,18 @@ class Car {
     this._color = color;
   }
 
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
   cloneCar() {
     const newCar = new Car();
     newCar._brand = this._brand;
