609b8da565e68f5c4cfb41a1ea4368e690da6b97
erickadikah <erickadikah2030@gmail.com>
2023-03-25 00:25:00 +0300

    task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,4 @@
-class HolbertonClass {
+export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -16,13 +16,12 @@ class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export default HolbertonClass;
-
-class StudentHolberton extends HolbertonClass {
-  constructor(firstName, lastName) {
-    super(HolbertonClass);
+export class StudentHolberton extends HolbertonClass {
+  constructor(firstName, lastName, holbertonClass) {
+    super(holbertonClass.year, holbertonClass.location);
     this._firstName = firstName;
     this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -44,4 +43,5 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
