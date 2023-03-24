f0eab589d5d2dd220c3a3e28b65b50478a3b951d
erickadikah <erickadikah2030@gmail.com>
2023-03-24 11:03:40 +0300

    advance task

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._brand = brand;
+    this._color = color;
+    this._motor = motor;
+    this._range = range;
+  }
+
+  cloneCar() {
+    const NewCar = super.constructor || super[Symbol.species];
+    return NewCar;
+  }
+}
+
+export default EVCar;
