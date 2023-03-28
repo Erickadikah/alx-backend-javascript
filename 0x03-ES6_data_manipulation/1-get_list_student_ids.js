31cd397f6ebcc7b49987a42683c4cbc6b52edfc7
erickadikah <erickadikah2030@gmail.com>
2023-03-28 17:24:47 +0300

    Data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,13 @@
 export default function getListStudentIds(obj) {
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    	{ id: 2, firstName: 'James', location: 'Columbia' },
-    	{ id: 5, firstName: 'Serena', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   const mappedstudents = students.map((student) => student.id);
   console.log(mappedstudents);
   if (Array.isArray(obj)) {
-    	return mappedstudents;
+    return mappedstudents;
   }
   return [];
 }
