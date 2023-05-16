4c718b8ba0e818f0bb5ee43ed602abb4ae2caa10
erickadikah <erickadikah2030@gmail.com>
2023-05-16 18:23:43 +0300

    reading file async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,10 +1,11 @@
+const fs = require('fs');
+
 async function countStudents(filepath) {
-    const fs = require('fs');
-    try {
-       const data = await fs.promises.readFile(filepath, 'utf-8');
+  try {
+    const data = await fs.promises.readFile(filepath, 'utf-8');
     if (!data) {
-        console.error('Cannot load the database')
-        return;
+      console.error('Cannot load the database');
+      return;
     }
     // console.log(data) debug getting binary =>  changed to('utf8')
     const lines = data.split('\n').filter((line) => line.trim().length > 0);
@@ -19,8 +20,7 @@ async function countStudents(filepath) {
     console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweFirstNames.join(', ')}`);
   } catch (err) {
     console.error('Error reading file:', err);
-  }  
-
+  }
 }
 
-module.exports = countStudents
+module.exports = countStudents;
