e52b3d5bd9e68fe795609449cfe69b091085b11c
erickadikah <erickadikah2030@gmail.com>
2023-05-16 12:06:44 +0300

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,10 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (name) => {
   // const name = process.stdin.read()
   // if (name !== null) {
-  console.log(`Your name is: ${name}`);
+  // console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}`);
   // }
 });
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
