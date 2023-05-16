8aad2de927a3e5cc9c29e5eb7f5ed0ce9e8d5fde
erickadikah <erickadikah2030@gmail.com>
2023-05-16 16:35:14 +0300

    try fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,19 +2,22 @@ const fs = require('fs');
 
 function countStudents(filepath) {
   try {
-    const data = fs.readFileSync(filepath, 'utf8');
+    // read data
+    const data = fs.readFileSync(filepath, 'utf-8');
     // console.log(data)
-    const numStd = (data).split('\n');
+    const numStd = data.split('\n');
     const stdNum = numStd.length - 1;
+
     console.log(`Number of students: ${stdNum}`);
+
     const csStudents = numStd.filter((numStds) => numStds.endsWith('CS'));
     // console.log(typeof(num_std[1])
     // console.log(typeof(csStudents))
     csStudents.forEach((student, index) => {
       csStudents[index] = student.split(',')[0].trim();
     });
-    // console.log(csStudents.length)
     console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+
     const sweStudents = numStd.filter((numStds) => numStds.endsWith('SWE'));
     sweStudents.forEach((student, index) => {
       sweStudents[index] = student.split(',')[0].trim();
