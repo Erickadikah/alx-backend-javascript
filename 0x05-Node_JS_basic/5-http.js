092e38bf519a6b0dc6e36f2eb0561bd9463c5a71
erickadikah <erickadikah2030@gmail.com>
2023-05-16 22:15:41 +0300

    http request Nodejs

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,47 +1,57 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const server = http.createServer((req, res) => {
-    if (req.url == '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello Holberton School!');
-    }else if (req.url == '/students') {
-        fs.readFile('database.csv', 'utf-8', (err, data) => {
-            if (err) {
-             console.error('Error reading file:', err);
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('Internal Server Error');
-            return;  
-            }
-            const lines = data.split('\n').filter((line) => line.trim().length > 0);
-        console.log(`Number of students: ${lines.length}`);
-
-        const csStudents = lines.filter((line) => line.endsWith('CS'));
-        const csFirstNames = csStudents.map((student) => student.split(',')[0].trim());
-        console.log(`Number of students in CS: ${csStudents.length}. List: ${csFirstNames.join(', ')}`);
-
-        const sweStudents = lines.filter((line) => line.endsWith('SWE'));
-        const sweFirstNames = sweStudents.map((student) => student.split(',')[0].trim());
-        // console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweFirstNames.join(', ')}`);
-        const output = `This is the list of our students\nNumber of students: ${lines.length}\nNumber of students in CS: ${csStudents.length}. List: ${csFirstNames.join(', ')}\nNumber of students in SWE: ${sweStudents.length}. List: ${sweFirstNames.join(', ')}`;
-
-        console.log(output);
-        res.statusCode == 200;
-        res.setHeader('Content-Type', 'text/plain')
-        res.end(output);
-        })
-    }else {
-      res.statusCode = 404;
+  if (req.url === '/') {
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found');  
-    }
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    fs.readFile('database.csv', 'utf-8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+        return;
+      }
+      console.log('This is the list of our students');
+      const lines = data.split('\n');
+      const students = lines.length - 1;
+      console.log(`Number of students: ${students}`);
+
+      const csStudents = lines.filter((line) => line.endsWith('CS'));
+      csStudents.forEach((student, index) => {
+        csStudents[index] = student.split(',')[0].trim();
+      });
+      console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+
+      const sweStudents = lines.filter((line) => line.endsWith('SWE'));
+      sweStudents.forEach((student, index) => {
+        sweStudents[index] = student.split(',')[0].trim();
+      });
+      console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+
+      const responseBody = `
+        Number of students: ${students}
+        Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}
+        Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}
+      `;
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(responseBody);
     });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
 
 server.listen(port, hostname, () => {
-    console.log(`server running at http://${hostname}:${port}/`)
+  console.log(`server running at http://${hostname}:${port}/`);
 });
