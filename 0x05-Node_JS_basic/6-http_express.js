b25b7a5ed039b71c75ae6012b960a135d7d51130
erickadikah <erickadikah2030@gmail.com>
2023-05-16 23:15:36 +0300

    express http request

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const app = express();
 // localhost
 const hostname = '127.0.0.1';
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('content-Type', 'text/plain');
-    res.send('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
