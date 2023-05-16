15333b73735b36903f2aa349290c663538fb8386
erickadikah <erickadikah2030@gmail.com>
2023-05-16 16:59:30 +0300

    fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,17 @@
 const http = require('http');
 
-//localhost
+// localhost
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
