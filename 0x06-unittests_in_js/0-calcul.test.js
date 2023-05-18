3bda3736b11d1db04e9778a270d95a39c4b4ca4a
erickadikah <erickadikah2030@gmail.com>
2023-05-18 23:41:45 +0300

    test case

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,17 +5,8 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function () {
   it('should calculate 1 + 3 = 4', function(){
     assert.equal(calculateNumber(1, 3), 4);
-  });
-
-  it('should calculate 1 + 3.7 = 5', function(){
     assert.equal(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should calculate 1.2 + 3.7 = 5', function(){
     assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should calculate 1.5 + 3.7 = 6', function(){
     assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-  });
+});
+});
