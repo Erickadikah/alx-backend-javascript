152045ff3d5b01d73673fb9c40a2982cda8a867d
erickadikah <erickadikah2030@gmail.com>
2023-05-19 02:31:58 +0300

    payment with spy and chai

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -3,11 +3,6 @@
 const Utils = {
     calculateNumber: function (type, a, b){
         if (type == 'SUM') return (Math.round(a) + Math.round(b));
-    //     if (type == 'SUBTRACT') return (Math.round(a) - Math.round(b));
-    //     if (type == 'DIVIDE'){
-    //         if (Math.round(b) == 0) return ('Error')
-    //     return (Math.round(a) / Math.round(b));
-    // }
     }
 };
 
