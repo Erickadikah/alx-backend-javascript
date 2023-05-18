e75376538c2e3f284b8a00c8e6dd89cc13531c7e
erickadikah <erickadikah2030@gmail.com>
2023-05-18 23:55:20 +0300

    unitest testcase

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,12 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('should calculate 1 + 3 = 4', function(){
+describe("claculateNumber", function() {
+  it("Should round the variables and return the sum", function() {
+    assert.equal(calculateNumber(2.1, 3.4), 5);
     assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1.4, 4.5), 6);
     assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-});
+    assert.equal(calculateNumber(0, 3.7), 4);
+  });
 });
