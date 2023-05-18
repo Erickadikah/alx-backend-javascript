e58cbd14f0a3e65568a9bbecbccdcd3da8ea3379
erickadikah <erickadikah2030@gmail.com>
2023-05-18 23:32:32 +0300

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -15,7 +15,7 @@ describe('calculateNumber', function () {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should calculate 1 + 3 = 4', function(){
-    assert.equal(calculateNumber(1, 3), 4);
+  it('should calculate 1.5 + 3.7 = 4', function(){
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
   });
