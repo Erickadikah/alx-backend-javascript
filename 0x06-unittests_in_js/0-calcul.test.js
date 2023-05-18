2db09857baebe8831f0930c8966f04fb49ac06e3
erickadikah <erickadikah2030@gmail.com>
2023-05-18 20:45:46 +0300

    calculate number test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('should calculate the sum of two numbers', () => {
-    const result = calculateNumber(1, 3);
-    assert.strictEqual(result, 4);
+describe('calculateNumber', function () {
+  it('should calculate 1 + 3 = 4', function(){
+    assert.equal(calculateNumber(1, 3), 4);
   });
 
-});
+  it('should calculate 1 + 3.7 = 5', function(){
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should calculate 1.2 + 3.7 = 5', function(){
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should calculate 1 + 3 = 4', function(){
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  });
