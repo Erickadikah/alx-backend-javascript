4b53060cff4e2582a8bcffada717ded5b7c59bce
erickadikah <erickadikah2030@gmail.com>
2023-05-19 02:12:58 +0300

    payment with spy and chai

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require("sinon");
+const Utils = require('./utils');
+const chai = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+
+const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should log the correct total', function() {
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    chai.expect(spy.calledWith('The total is: 120')).to.be.true;
+    chai.expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    spy.restore(); // Restore the spy after the test
+  });
+});
+
+// sendPaymentRequestToApi(100, 20);
+// console.log(calculateNumberSpy.calledWith('SUM', 100, 20));
