4b1dc9f2c5dfe8278b8df7997d29eaf826a9a56a
erickadikah <erickadikah2030@gmail.com>
2023-05-19 22:07:27 +0300

    regex intergration test

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,73 +1,66 @@
-const chai = require("chai");
-const expect = chai.expect;
-const request = require('supertest');
-const app = require('./api');
+const { expect } = require('chai');
+const request = require('request');
 
-describe('Index Page', function() {
-  it('should have correct status code 200', function(done) {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end(function(err, res) {
-        if (err) {
-          done(err); // Handle the error and pass it to done()
-        } else {
-          done(); // Test passed, call done() without an error
-        }
-      });
+describe('Index Page', () => {
+  it('should respond with the correct status code', () => {
+    request('http://localhost:7865', (error, res, body) => {
+      expect(res.statusCode).to.equal(200);
+    });
   });
 
-  it('should have the correct Content-Type', function(done) {
-    request(app)
-      .get('/')
-      .end(function(err, res) {
-        if (err) {
-          done(err);
-        } else {
-          expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
-          done();
-        }
-      });
+  it('should have the correct content of the body', () => {
+    request('http://localhost:7865', (error, res, body) => {
+      expect(body).to.contain('Welcome to the payment system');
+    });
   });
 
-  it('should have the correct Content-Length', function(done) {
-    request(app)
-      .get('/')
-      .end(function(err, res) {
-        if (err) {
-          done(err);
-        } else {
-          expect(res.headers['content-length']).to.equal('29');
-          done();
-        }
-      });
+  it('should have the corrent Content-Type', () => {
+    request('http://localhost:7865', (error, res, body) => {
+      expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
+    });
   });
 
-  it('should return 404 status code when :id is not a number', function(done) {
-    const id = 'abc';
-    request(app)
-      .get(`/cart/${id}`)
-      .expect(404)
-      .end(function(err, res) {
-        if (err) {
-          done(err);
-        } else {
-          done();
-        }
-      });
+  it('should have the corrent Content-Length', () => {
+    request('http://localhost:7865', (error, res, body) => {
+      expect(res.headers['content-length']).to.equal('29');
+    });
+  });
+});
+
+describe('Cart Page', () => {
+  it('should have correst Status Code with num id param', () => {
+    request('http://localhost:7865', (error, res, body) => {
+      expect(res.statusCode).to.equal(200);
+    });
   });
 
-  it('should return statusCode 200 when :id is a number', function(done) {
-    const id = 123;
-    request(app)
-      .get(`/cart/${id}`)
-      .expect(200)
-      .end(function(err, res) {
-        if (err) {
-          done(err);
-        } else {
-          done();
-        }
-      });
+  it('should have the correct result with number id parameter', () => {
+    request('http://localhost:7865/cart/12', (error, res, body) => {
+      expect(body).to.contain('Payment methods for cart 12');
+    });
+  });
+
+  it('should have the correct status code when non number id parameter is provided', () => {
+    request('http://localhost:7865/cart/hello', (error, res, body) => {
+      expect(res.statusCode).to.equal(404);
+    });
+  });
+
+  it('should return the correct content-type given valid id parameter', () => {
+    request('http://localhost:7865/cart/12', (error, res, body) => {
+      expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
+    });
+  });
+
+  it('should return the correct content in the body when non number id is provided', () => {
+    request('http://localhost:7865/cart/hello', (error, res, body) => {
+      expect(body).to.contain('Cannot GET /cart/hello');
+    });
+  });
+
+  it('should return the correct content length', () => {
+    request('http://localhost:7865/cart/12', (error, res, body) => {
+      expect(res.headers['content-length']).to.equal('27');
+    });
   });
 });
