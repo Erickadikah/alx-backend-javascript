b67efb465dc382983bca0c5e427042430803e12c
erickadikah <erickadikah2030@gmail.com>
2023-05-21 22:26:32 +0300

    task read data async

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,30 +1,32 @@
 const http = require('http');
 // const fs = require('fs');
-const readFileSync = require('./2-read_file');
+const countStudents = require('./3-read_file_async');
+// const { count } = require('console');
 // const stdin = process.stdin
 
+
+// const database = require('./database.csv');
 const hostname = '127.0.0.1';
 const port = 1245;
-const database = process.argv[2];
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async(req, res) => {
   res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
     res.statusCode = 200;
-    res.end('Hello Holberton School!');
+    res.write('Hello Holberton School!');
   } else if (req.url === '/students') {
     // message = readFileSync(database);
     res.write('This is the list of our students\n');
-    res.statusCode = 200;
-    // message = fs
-    // readFileSync(database);
-    // send message to client && data to client
-    res.end(readFileSync(database));
-  } else {
-    res.statusCode = 404;
-    res.end('This is not a valid URL');
+    try {
+      const data = await countStudents(process.argv[2], res);
+      res.end(`${data}`)
+
+    } catch (error) {
+      res.end(error.message)
+    }
   }
+    
 });
 
 app.listen(port, hostname, () => {
